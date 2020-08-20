41a6d9b664238e3ec329160fc32d4dccb9e2bcc7
priyajee <priyajee@twitch.tv>
2020-08-19 20:20:50 -0700

    Add decode and verify

--- README.md
+++ README.md
@@ -4,15 +4,22 @@ run `yarn install`
 # Usage
 
 ```
-./index.js --privateKey=../test.pem --payload.foo=bar --payload.tom=cat
+./index.js --privateKey=../private.pem --publicKey=../public.pem --payload.foo=bar --payload.tom=cat
 
-=============== Payload ==============
-{
-  "foo": "bar",
-  "tom": "cat"
-}
+------------------- Payload -----------------
+{"foo":"bar","tom":"cat"}
+------------------- Payload -----------------
 
-================= JTW ================
-eyJhbGciOiJFUzM4NCIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJ0b20iOiJjYXQiLCJpYXQiOjE1OTc4ODg3NTF9.mOvjzrJpiIxsEQ40tGyip6wLioTJyUZJIWTYubfKpV78djakmeC3C1yDj1XYMEjtfd51VRaZIOSb6vA_9bv2_3J0R92ksKsZW0Fe6cGHhkd2pQAODSsCluWfuMcO__Rp
+--------------------- JTW -------------------
+eyJhbGciOiJFUzM4NCIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJ0b20iOiJjYXQiLCJpYXQiOjE1OTc4OTM2MDZ9.Gsy83CvY50QCInNOkGe0GdMNpoaqGUlZ1mPsW1w6C2DQniKhba4Csm-K8j0WFLYMXAic3Ur1hbGTKuE1uXJZy1wNl15tzvkF91E0US42KMtoCadiHP3dRzDjvpZe3nqd
+--------------------- JTW -------------------
+
+------------------- Decode ------------------
+{ foo: 'bar', tom: 'cat', iat: 1597893606 }
+------------------- Decode ------------------
+
+------------------- Verify ------------------
+{ foo: 'bar', tom: 'cat', iat: 1597893606 }
+------------------- Verify ------------------
 ```
 
--- index.js
+++ index.js
@@ -5,10 +5,27 @@ const fs = require("fs");
 const { argv } = require("yargs");
 const path = require("path");
 
+let publicKey = null;
+if (argv.publicKey) {
+    publicKey = fs.readFileSync(path.resolve(argv.publicKey));
+}
 const privateKey = fs.readFileSync(path.resolve(argv.privateKey));
 const { payload } = argv;
+const token = jwt.sign(payload, privateKey, { algorithm: "ES384" });
 
-console.log("\n=============== Payload ==============");
-console.log(JSON.stringify(payload, null, 2));
-console.log("\n================= JTW ================");
-console.log(jwt.sign(payload, privateKey, { algorithm: "ES384" }));
+console.log("\n------------------- Payload -----------------");
+console.log(JSON.stringify(payload));
+console.log("------------------- Payload -----------------");
+console.log("\n--------------------- JTW -------------------");
+console.log(token);
+console.log("--------------------- JTW -------------------");
+
+if (publicKey) {
+    console.log("\n------------------- Decode ------------------");
+    console.log(jwt.decode(token));
+    console.log("------------------- Decode ------------------");
+
+    console.log("\n------------------- Verify ------------------");
+    console.log(jwt.verify(token, publicKey, { algorithms: ["ES384"] }));
+    console.log("------------------- Verify ------------------\n");
+}
